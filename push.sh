f96b55c17e5f637a969bec5f0c7443a61be21a96
Saiprasad Toshatwad <77664251+dev-hack95@users.noreply.github.com>
2022-11-07 21:34:52 +0530

    Add files via upload

---
+++ push.sh
@@ -0,0 +1,9 @@
+git init
+git config --global user.email "saiprasadtoshatwad@gmail.com"
+git config --global user.name "dev-hack95"
+git branch -M main
+git remote add origin https://github.com/dev-hack95/Diabetes_detection
+git add .
+git commit -m "update"
+git push origin main
+echo "Push Successful"
