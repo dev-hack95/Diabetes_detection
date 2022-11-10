aef4c50a94839e75de4c3818c58e809744a583f5
Saiprasad Toshatwad <77664251+dev-hack95@users.noreply.github.com>
2022-11-10 12:22:56 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-git init
-git config --global user.email "saiprasadtoshatwad@gmail.com"
-git config --global user.name "dev-hack95"
-git branch -M main
-git remote add origin https://github.com/dev-hack95/Diabetes_detection
-git add .
-git commit -m "update"
-git push origin main
-echo "Push Successful"
